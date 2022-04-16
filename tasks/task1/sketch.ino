5b6daacb27527a42e2f341f0679fc370d5606361
Niatomi <playervoker@gmail.com>
2022-04-16 20:33:46 +0400

    Add rus documentation and polish code

--- tasks/task1/sketch.ino
+++ tasks/task1/sketch.ino
@@ -1,14 +1,24 @@
 #define LEDPIN 13
+#define BUTTON 3
 
 volatile boolean actionState = LOW;
 
+/**
+ * Инициализация
+ * 
+ * Добавляем режим работы для кнопки
+ * В качестве методы прерывания исползьуется lightBlinking
+ */
 void setup() {
     Serial.begin(9600);
     pinMode(LEDPIN, OUTPUT);
-    pinMode(3, INPUT_PULLUP);
+    pinMode(BUTTON, INPUT_PULLUP);
     attachInterrupt(1, lightBlinking, RISING);
 }
 
+/*
+* Метод для мигания светодиода
+*/
 void lightBlinking() {
     
     ledVarChange();
@@ -16,11 +26,17 @@ void lightBlinking() {
 
 }
 
+/*
+* Изменяет значение на светодиоде
+*/
 static void ledVarChange() {
     actionState = !actionState;
     digitalWrite(LEDPIN, actionState);
 }
 
+/*
+* Метод для избежания дрезбезга контактов
+*/
 void waitMicros() {
     for (int i = 0; i < 200; i++) {
         
